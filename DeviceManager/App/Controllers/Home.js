e0fdd7750c3ae78e884a7f9d4685564917da1d3e
WenboLi-Findly <wenbo.li@findly.com>
2014-08-29 20:48:27 +1200

    Created Project base

---
+++ DeviceManager/App/Controllers/Home.js
@@ -0,0 +1,28 @@
+﻿
+deviceManager.controller('HomeCtrl',
+    ['$scope',
+    function ($scope) {
+        var client = new WindowsAzure.MobileServiceClient('https://mcom-device-list.azure-mobile.net/', 'fevVWOAOauhjDswvLdefrcWMQgUuOi19');
+        var deviceTable = client.getTable('Device');
+
+        $scope.devices = [];
+        $scope.errorMessage = "";
+
+        function reload() {
+            var query = deviceTable.where({});
+
+            query.read().then(function (devices) {
+                $scope.devices = devices;
+                $scope.$apply();
+            }, handleError);
+        }
+
+        function handleError(error) {
+            var text = error + (error.request ? ' - ' + error.request.status : '');
+            $scope.errorMessage = text;
+        }
+
+        reload();
+
+
+    }]);
---
+++ DeviceManager/App/Main.js
@@ -0,0 +1,9 @@
+﻿window.deviceManager = angular.module('deviceManager', ['ngRoute']);
+
+// Configure routes
+deviceManager.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.
+        when('/', { templateUrl: 'App/Templates/Home.html', controller: 'HomeCtrl' }).
+        when('/home', { templateUrl: 'App/Templates/Home.html', controller: 'HomeCtrl' }).
+        otherwise({ redirectTo: '/home' });
+}]);
